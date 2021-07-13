292f5942b7732d0a873d5df56ea2d685f3fe0ae7
B Tasker <github@bentasker.co.uk>
2021-07-13 18:32:35 +0100

    Prepare for automated subbing.
    That's enough for tonight though, time to walk the dog

--- bootstrap_acct.py
+++ bootstrap_acct.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 #
 #
-
-
 import requests
 import reddit
 import getpass
@@ -24,3 +22,10 @@ red.request_path("/api/v1/me")
 print()
 print(red.subscribe_to_sub('/r/supportlol'))
 print(red.unsubscribe_from_sub('/r/supportlol'))
+
+# This means that we can now iterate over a list of subs and sub to them
+subs = [] # TODO: Fetch these from an input file
+
+for sub in subs:
+    red.subscribe_to_sub("/r/{}".format(sub))
+    
